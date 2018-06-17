f94dcb34782b7e72bbb960e621ce0cec9895c781
Tracie <traciechang@hotmail.com>
2018-06-17 16:31:54 -0700

    created editor

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,18 +1,35 @@
 import React from "react";
+import brace from "brace";
+import AceEditor from "react-ace";
+
+import 'brace/mode/ruby';
+import 'brace/theme/monokai';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
 
         this.logout = this.props.logout.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(newValue) {
+        console.log('change',newValue);
     }
 
     render() {
-        console.log("hitting Home render!!")
         return (
-            <div>"Howdy!
+            <div>
 
                 <button onClick={this.logout}>Log Out</button>
+
+                <AceEditor
+                    mode="ruby"
+                    theme="monokai"
+                    onChange={this.onChange}
+                    fontSize={14}
+                    name="myrubee"
+                    editorProps={{$blockScrolling: true}} />
             </div>
         )
     }
