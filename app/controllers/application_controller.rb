1c074ba9f0577bcd8c9a68a03a82e674e8b78edc
Tracie <traciechang@hotmail.com>
2018-07-13 18:53:55 -0700

    add window, preloaded state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   private
   def current_user
     return nil unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
   end
 
   def login(user)
